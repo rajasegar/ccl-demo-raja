1c230d0608e3258c48d9024a8b01d64471bb56f5
Duncan Bayne <duncan@bayne.id.au>
2018-06-07 22:44:37 +1000

    Add logging to figure out directory contents

--- heroku-compile.lisp
+++ heroku-compile.lisp
@@ -1,3 +1,4 @@
 ;; TODO: duncan@bayne.id.au: HACK: fix this
+(mapcar 'print (directory (make-pathname :directory '(:relative ".") :name :wild))
 (load "./system.asd")
 (ql:quickload :heroku-app-common-lisp)
