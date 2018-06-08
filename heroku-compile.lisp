499bdf82a3d95f19acb4bece7c3a51fc5a3f0555
Duncan Bayne <duncan@bayne.id.au>
2018-06-08 10:05:29 +1000

    Remove logging and ASDF load hack

--- heroku-compile.lisp
+++ heroku-compile.lisp
@@ -1,4 +1,2 @@
 ;; TODO: duncan@bayne.id.au: HACK: fix this
-(mapcar 'print (directory (make-pathname :directory '(:relative ".") :name :wild)))
-(load "./system.asd")
 (ql:quickload :heroku-app-common-lisp)
